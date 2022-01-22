ad4202167ebd0910ab54ae48c1bdd6b973453f03
burmau garba <garbab1@nku.edu>
2022-01-22 04:09:11 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by DJ on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
